d9deb82812e4700d9107f45cfac1c8f32d708acb
Restless Pikachu <58870378+sovieticwolf@users.noreply.github.com>
2019-12-14 08:15:58 -0400

    Add files via upload
    this is peggama , a program for help in linear algebra , is so easy to use , and makes cramer 3x3 .

---
+++ peggama.py
@@ -0,0 +1,316 @@
+
+
+#pegasus v0.1
+#made by red wolf 
+#at 16/10/2019
+#only for use of students of UNEXPOC 
+
+
+def modulos():
+    print("bienvenido a la seccion de modulos ")
+    print("quieres hacer los cuadros o obtener un \n modulo directo? ")
+    print("modulo por cuadros >>>1")
+    print("modulo directo     >>>2")
+    seleccion=int(input(">>>"))
+    if (seleccion==1):
+        print("elegiste hacerlo por cuadros")
+        modulo=int(input("introduce el modulo >>>"))
+        
+        print("cuadro de sumas ")
+        print("+   0   1   2   3   4   5   6   7   8   9")
+        a1=0
+        a2=1%modulo
+        a3=2%modulo
+        a4=3%modulo
+        a5=4%modulo
+        a6=5%modulo
+        a7=6%modulo
+        a8=7%modulo
+        a9=8%modulo
+        a10=9%modulo
+        print("0  ",a1," ",a2," ",a3," ",a4," ",a5," ",a6," ",a7," ",a8," ",a9," ",a10)
+        b1=1%modulo
+        b2=2%modulo
+        b3=3%modulo
+        b4=4%modulo
+        b5=5%modulo
+        b6=6%modulo
+        b7=7%modulo
+        b8=8%modulo
+        b9=9%modulo
+        b10=10%modulo
+        print("1  ",b1," ",b2," ",b3," ",b4," ",b5," ",b6," ",b7," ",b8," ",b9," ",b10)
+        c1=2%modulo
+        c2=3%modulo
+        c3=4%modulo
+        c4=5%modulo
+        c5=6%modulo
+        c6=7%modulo
+        c7=8%modulo
+        c8=9%modulo
+        c9=10%modulo
+        c10=11%modulo
+        print("2  ",c1," ",c2," ",c3," ",c4," ",c5," ",c6," ",c7," ",c8," ",c9," ",c10)
+        d1=3%modulo
+        d2=4%modulo
+        d3=5%modulo
+        d4=6%modulo
+        d5=7%modulo
+        d6=8%modulo
+        d7=9%modulo
+        d8=10%modulo
+        d9=11%modulo
+        d10=12%modulo
+        print("3  ",d1," ",d2," ",d3," ",d4," ",d5," ",d6," ",d7," ",d8," ",d9," ",d10)
+        e1=4%modulo
+        e2=5%modulo
+        e3=6%modulo
+        e4=7%modulo
+        e5=8%modulo
+        e6=9%modulo
+        e7=10%modulo
+        e8=11%modulo
+        e9=12%modulo
+        e10=13%modulo
+        print("4  ",e1," ",e2," ",e3," ",e4," ",e5," ",e6," ",e7," ",e8," ",e9," ",e10)
+        f1=5%modulo
+        f2=6%modulo
+        f3=7%modulo
+        f4=8%modulo
+        f5=9%modulo
+        f6=10%modulo
+        f7=11%modulo
+        f8=12%modulo
+        f9=13%modulo
+        f10=14%modulo
+        print("5  ",f1," ",f2," ",f3," ",f4," ",f5," ",f6," ",f7," ",f8," ",f9," ",f10)      
+        g1=6%modulo
+        g2=7%modulo
+        g3=8%modulo
+        g4=9%modulo
+        g5=10%modulo
+        g6=11%modulo
+        g7=12%modulo
+        g8=13%modulo
+        g9=14%modulo
+        g10=15%modulo
+        print("6  ",g1," ",g2," ",g3," ",g4," ",g5," ",g6," ",g7," ",g8," ",g9," ",g10)
+        h1=7%modulo
+        h2=8%modulo
+        h3=9%modulo
+        h4=10%modulo
+        h5=11%modulo
+        h6=12%modulo
+        h7=13%modulo
+        h8=14%modulo
+        h9=15%modulo
+        h10=16%modulo
+        print("7  ",h1," ",h2," ",h3," ",h4," ",h5," ",h6," ",h7," ",h8," ",h9," ",h10)
+        i1=8%modulo
+        i2=9%modulo
+        i3=10%modulo
+        i4=11%modulo
+        i5=12%modulo
+        i6=13%modulo
+        i7=14%modulo
+        i8=15%modulo
+        i9=16%modulo
+        i10=17%modulo
+        print("8  ",i1," ",i2," ",i3," ",i4," ",i5," ",i6," ",i7," ",i8," ",i9," ",i10)
+        j1=9%modulo
+        j2=10%modulo
+        j3=11%modulo
+        j4=12%modulo
+        j5=13%modulo
+        j6=14%modulo
+        j7=15%modulo
+        j8=16%modulo
+        j9=17%modulo
+        j10=18%modulo
+        print("9  ",j1," ",j2," ",j3," ",j4," ",j5," ",j6," ",j7," ",j8," ",j9," ",j10)
+        #pendiente con este beta c[:v>
+        print(" cuadro de restas ")
+        print("-   0   1   2   3   4   5   6   7   8   9")
+        a1=0
+        a2=(0-1)%modulo
+        a3=(0-2)%modulo
+        a4=(0-3)%modulo
+        a5=(0-4)%modulo
+        a6=(0-5)%modulo
+        a7=(0-6)%modulo
+        a8=(0-7)%modulo
+        a9=(0-8)%modulo
+        a10=(0-9)%modulo
+        print("0  ",a1," ",a2," ",a3," ",a4," ",a5," ",a6," ",a7," ",a8," ",a9," ",a10)
+        b1=(1-0)%modulo
+        b2=(1-1)%modulo
+        b3=(1-2)%modulo
+        b4=(1-3)%modulo
+        b5=(1-4)%modulo
+        b6=(1-5)%modulo
+        b7=(1-6)%modulo
+        b8=(1-7)%modulo
+        b9=(1-8)%modulo
+        b10=(1-9)%modulo
+        print("1  ",b1," ",b2," ",b3," ",b4," ",b5," ",b6," ",b7," ",b8," ",b9," ",b10)
+        c1=(2-0)%modulo
+        c2=(2-1)%modulo
+        c3=(2-2)%modulo
+        c4=(2-3)%modulo
+        c5=(2-4)%modulo
+        c6=(2-5)%modulo
+        c7=(2-6)%modulo
+        c8=(2-7)%modulo
+        c9=(2-8)%modulo
+        c10=(2-9)%modulo
+        print("2  ",c1," ",c2," ",c3," ",c4," ",c5," ",c6," ",c7," ",c8," ",c9," ",c10)
+        d1=(3-0)%modulo
+        d2=(3-1)%modulo
+        d3=(3-2)%modulo
+        d4=(3-3)%modulo
+        d5=(3-4)%modulo
+        d6=(3-5)%modulo
+        d7=(3-6)%modulo
+        d8=(3-7)%modulo
+        d9=(3-8)%modulo
+        d10=(3-9)%modulo
+        print("3  ",d1," ",d2," ",d3," ",d4," ",d5," ",d6," ",d7," ",d8," ",d9," ",d10)
+        e1=(4-0)%modulo
+        e2=(4-1)%modulo
+        e3=(4-2)%modulo
+        e4=(4-3)%modulo
+        e5=(4-4)%modulo
+        e6=(4-5)%modulo
+        e7=(4-6)%modulo
+        e8=(4-7)%modulo
+        e9=(4-8)%modulo
+        e10=(4-9)%modulo
+        print("4  ",e1," ",e2," ",e3," ",e4," ",e5," ",e6," ",e7," ",e8," ",e9," ",e10)
+        f1=(5-0)%modulo
+        f2=(5-1)%modulo
+        f3=(5-2)%modulo
+        f4=(5-3)%modulo
+        f5=(5-4)%modulo
+        f6=(5-5)%modulo
+        f7=(5-6)%modulo
+        f8=(5-7)%modulo
+        f9=(5-8)%modulo
+        f10=(5-9)%modulo
+        print("5  ",f1," ",f2," ",f3," ",f4," ",f5," ",f6," ",f7," ",f8," ",f9," ",f10)      
+        g1=(6-0)%modulo
+        g2=(6-1)%modulo
+        g3=(6-2)%modulo
+        g4=(6-3)%modulo
+        g5=(6-4)%modulo
+        g6=(6-5)%modulo
+        g7=(6-6)%modulo
+        g8=(6-7)%modulo
+        g9=(6-8)%modulo
+        g10=(6-9)%modulo
+        print("6  ",g1," ",g2," ",g3," ",g4," ",g5," ",g6," ",g7," ",g8," ",g9," ",g10)
+        h1=(7-0)%modulo
+        h2=(7-1)%modulo
+        h3=(7-2)%modulo
+        h4=(7-3)%modulo
+        h5=(7-4)%modulo
+        h6=(7-5)%modulo
+        h7=(7-6)%modulo
+        h8=(7-7)%modulo
+        h9=(7-8)%modulo
+        h10=(7-9)%modulo
+        print("7  ",h1," ",h2," ",h3," ",h4," ",h5," ",h6," ",h7," ",h8," ",h9," ",h10)
+        i1=(8-0)%modulo
+        i2=(8-1)%modulo
+        i3=(8-2)%modulo
+        i4=(8-3)%modulo
+        i5=(8-4)%modulo
+        i6=(8-5)%modulo
+        i7=(8-6)%modulo
+        i8=(8-7)%modulo
+        i9=(8-8)%modulo
+        i10=(8-9)%modulo
+        print("8  ",i1," ",i2," ",i3," ",i4," ",i5," ",i6," ",i7," ",i8," ",i9," ",i10)
+        j1=(9-0)%modulo
+        j2=(9-1)%modulo
+        j3=(9-2)%modulo
+        j4=(9-3)%modulo
+        j5=(9-4)%modulo
+        j6=(9-5)%modulo
+        j7=(9-6)%modulo
+        j8=(9-7)%modulo
+        j9=(9-8)%modulo
+        j10=(9-9)%modulo
+        print("9  ",j1," ",j2," ",j3," ",j4," ",j5," ",j6," ",j7," ",j8," ",j9," ",j10)
+
+
+    elif(seleccion==2):
+        print("elegiste hacerlo directo")
+        start=int(input("introduce el modulo en el cual estas \n trabajando >>>"))
+        fin=int(input("introduce el numero a hallar el modulo>>>"))
+        respuesta=(fin%start)
+        print("el numero :",fin,", en modulo :",start,", es :",respuesta )
+
+
+    else:
+        ("elegiste la opcion incorrecta /n quieres salir del programa?")
+        hoshit=str(input("S o N >>>>"))
+        if(hoshit=="s"):
+            modulos()
+
+        else:
+            print("adios")
+
+
+
+def cramer():
+    print("cramer editor v1.0")
+    print("by red wolf 30/09/2019")
+    print("introduce las variables")
+    a1=int(input("introduce la variable x1 >"))
+    a2=int(input("introduce la variable y1 >"))
+    a3=int(input("introduce la variable z1 >"))
+    ar=int(input("introduce la variable R1 >"))
+    b1=int(input("introduce la variable x2 >"))
+    b2=int(input("introduce la variable y2 >"))
+    b3=int(input("introduce la variable z2 >"))
+    br=int(input("introduce la variable R2 >"))
+    c1=int(input("introduce la variable x3 >"))
+    c2=int(input("introduce la variable y3 >"))
+    c3=int(input("introduce la variable z3 >"))
+    cr=int(input("introduce la variable R3 >"))
+    print("el delta es :",(((a1*b2*c3)+(a2*b3*c1)+(a3*b1*c2))-((a3*b2*c1)+(a2*b1*c3)+(a1*b3*c2))))
+    delta=(((a1*b2*c3)+(a2*b3*c1)+(a3*b1*c2))-((a3*b2*c1)+(a2*b1*c3)+(a1*b3*c2)))
+    print("el delta X es:",((ar*b2*c3)+(a2*b3*cr)+(a3*br*c2)-((a3*b2*cr)+(a2*br*c3)+(ar*b3*c2))))
+    deltax=((ar*b2*c3)+(a2*b3*cr)+(a3*br*c2)-((a3*b2*cr)+(a2*br*c3)+(ar*b3*c2)))
+    print("el x es :",(deltax/delta))
+    print("el delta Y es:",((a1*br*c3)+(ar*b3*c1)+(a3*b2*cr)-((a3*br*c1)+(ar*b2*c3)+(a1*b3*cr))))
+    deltay=(a1*br*c3)+(ar*b3*c1)+(a3*b2*cr)-((a3*br*c1)+(ar*b2*c3)+(a1*b3*cr))
+    print("el y es :",(deltay/delta))
+    print("el delta Z es:",((a1*b2*cr)+(a2*br*c1)+(ar*b1*c2)-((ar*b2*c1)+(a2*b1*c2)+(a1*br*c2))))
+    deltaz=(a1*b2*cr)+(a2*br*c1)+(ar*b1*c2)-((ar*b2*c1)+(a2*b1*cr)+(a1*br*c2))
+    print("el z es :",(deltaz/delta))
+
+
+    final=input("el programa finalizo quieres intentarlo otra vez? \"s o n\" >>>")
+    if final=="s" :
+        print("bai")     
+    elif final=="n":
+        print("bai we")
+
+print("bienvenido al programa pegasus v0.1 \n este es un programa orientado a la \n ayuda en algebra lineal ")
+print("hecho por sovietic wolf")
+print("comenzado en el 16/10/2019")
+
+def programa():
+    print(" que quieres hacer? \n modulos o cramer?? \n modulos (1) \n cramer (2)")
+    seleccion=int(input("introduce tu eleccion >>>"))
+    if (seleccion==1):
+        modulos()
+        programa()
+    elif(seleccion==2):
+        cramer()
+        programa()
+    else:
+        print("elegiste la opcion incorrecta \n el programa finalizara ")
+
+programa()
